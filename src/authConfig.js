f1301f5185ad09b6e399cb097646b32c7c297386
Tabur2026 <Coordinated@tabur.sa>
2026-06-13 17:53:59 +0300

    Update authConfig.js

--- src/authConfig.js
+++ src/authConfig.js
@@ -3,7 +3,7 @@ export const msalConfig = {
     clientId: "30f3ceed-213b-4eab-94b5-8e210cc42bee",
     authority:
       "https://login.microsoftonline.com/ab79833e-417a-460e-9da3-37a526b866f1",
-    redirectUri: "http://localhost:5173",
+   redirectUri: window.location.origin,
   },
 };
 
